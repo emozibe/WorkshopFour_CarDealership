5bcd7276803a74c33ea865f505a63dda64d17137
emozibe <rashidtalal.02@gmail.com>
2024-10-24 15:02:19 -0500

    phase 1 complete

--- src/main/java/com/ps/Dealership.java
+++ src/main/java/com/ps/Dealership.java
@@ -1,4 +1,56 @@
 package com.ps;
 
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name,
+                      String address,
+                      String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
 }
--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -1,4 +1,16 @@
 package com.ps;
 
 public class UserInterface {
+    // private static Dealership dealership;
+
+    public static void init() {
+        System.out.println("Init method");
+        //Loading the dealership and inventory from a file
+    }
+
+    public static void display() {
+        init();
+        System.out.println("Load menu");
+        // Load the menu
+    }
 }
